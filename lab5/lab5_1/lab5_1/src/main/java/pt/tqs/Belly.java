fd381085cb2ef6bc5100264296e75abf80001c2e
cristiano-nicolau <cristianonicolau@ua.pt>
2024-03-14 19:03:56 +0000

    lab 5_1 completed

---
+++ lab5/lab5_1/lab5_1/src/main/java/pt/tqs/Belly.java
@@ -0,0 +1,9 @@
+package pt.tqs;
+
+public class Belly {
+    public void eat(int cukes) {}
+
+    public void wait(int time) {}
+
+    public void growl() {}
+}
---
+++ lab5/lab5_1/lab5_1/src/test/java/CalculatorSteps.java
@@ -0,0 +1,52 @@
+package pt.tqs;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CalculatorSteps {
+
+    private Calculator calc;
+
+    @Given("^a calculator I just turned on$")
+    public void setup() {
+        calc = new Calculator();
+    }
+
+    @When("^I add (\\d+) and (\\d+)$")
+    public void add(int arg1, int arg2) {
+        calc.push(arg1);
+        calc.push(arg2);
+        calc.push("+");
+    }
+
+    @When("^I substract (\\d+) to (\\d+)$")
+    public void substract(int arg1, int arg2) {
+        calc.push(arg1);
+        calc.push(arg2);
+        calc.push("-");
+    }
+
+    @When("^I multiply (\\d+) to (\\d+)$")
+    public void multiply(int arg1, int arg2){
+        calc.push(arg1);
+        calc.push(arg2);
+        calc.push("*");
+    }
+
+    @When("^I divide (\\d+) to (\\d+)$")
+    public void divide(int arg1, int arg2){
+        calc.push(arg1);
+        calc.push(arg2);
+        calc.push("/");
+    }
+
+    @Then("^the result is (\\d+)$")
+    public void the_result_is(double expected) {
+        Number value = calc.value();
+        assertEquals(expected, value);
+    }
+
+}
